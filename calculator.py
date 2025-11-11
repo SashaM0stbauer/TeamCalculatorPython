c220799414e7a6fd7fdf94153f0aef7d123813f9
SashaM0stbauer <mr.vineter@gmail.com>
2025-11-11 18:37:00 +0200

    Add Calculator class with subtract method
    Implemented the subtract method in the Calculator class.

---
+++ calculator.py
@@ -0,0 +1,8 @@
+class Calculator:
+    def add(self, a, b):
+        """Метод буде реалізований учасником 2"""
+        pass
+
+    def subtract(self, a, b):
+        """Метод реалізує Team Lead"""
+        return a - b
